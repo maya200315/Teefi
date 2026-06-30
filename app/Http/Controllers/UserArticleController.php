2887d1cfd1027feb15cdeb9a3e8fa5ce5dc7dbaa
lana-lababidy <lanalaba8@gmail.com>
2026-06-30 03:16:12 +0300

    UserArticleController

---
+++ app/Http/Controllers/UserArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class UserArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::orderBy('datetime', 'desc')
+            ->get(['id', 'title', 'content', 'datetime']);
+
+        return response()->json([
+            'status' => true,
+            'data'   => $articles
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        $article = Article::find($id, ['id', 'title', 'content', 'datetime']);
+
+        if (!$article) {
+            return response()->json([
+                'status'  => false,
+                'message' => 'Article not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data'   => $article
+        ]);
+    }
+}
